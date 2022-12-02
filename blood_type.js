be1789f0fd87cf1850c25bda78af8568521e6a49
kennethjy <114073455+kennethjy@users.noreply.github.com>
2022-12-02 18:01:16 +0700

    Update blood_type.js

--- blood_type.js
+++ blood_type.js
@@ -1,17 +1,17 @@
 function canGiveBlood(a, b){
-		if(a[a.length - 1] == "+" && b[b.length - 1] == "-"){
-			return false;
-		}
-		else if(b[0] == "O" && a[0] != "O"){
-			return false;
-		}
-		else if(b[0] == "B" && a[0] == "A"){
-			return false;
-		}
-		else if(b[0] == "A" && b[1] != "B" && (a[0] == "B" || (a[0] == "A" && a[1] == "B"))){
-			return false;
-		}
-		else{
-			return true;
-		}
+	if(a[a.length - 1] == "+" && b[b.length - 1] == "-"){
+		return false;
 	}
+	else if(b[0] == "O" && a[0] != "O"){
+		return false;
+	}
+	else if(b[0] == "B" && a[0] == "A"){
+		return false;
+	}
+	else if(b[0] == "A" && b[1] != "B" && (a[0] == "B" || (a[0] == "A" && a[1] == "B"))){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
