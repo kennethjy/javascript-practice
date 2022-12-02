0ca66461e84c8a853d9de5860fb73289920b2c30
kennethjy <114073455+kennethjy@users.noreply.github.com>
2022-12-02 18:01:52 +0700

    Update decipher.js

--- decipher.js
+++ decipher.js
@@ -1,11 +1,11 @@
 function digitalDecipher(eMessage, key){
-		for(let i = 0; i < eMessage.length; i++){
-			eMessage[i] -= parseInt(key.toString()[i % key.toString().length]);
-		}
-		const alphabet = "abcdefghijklmnopqrstuvwxyz";
-		var out = "";
-		for(let i = 0; i < eMessage.length; i++){
-			out += alphabet[eMessage[i] - 1];
-		}
-		return out;
+	for(let i = 0; i < eMessage.length; i++){
+		eMessage[i] -= parseInt(key.toString()[i % key.toString().length]);
 	}
+	const alphabet = "abcdefghijklmnopqrstuvwxyz";
+	var out = "";
+	for(let i = 0; i < eMessage.length; i++){
+		out += alphabet[eMessage[i] - 1];
+	}
+	return out;
+}
